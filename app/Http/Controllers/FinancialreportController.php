664ca715899631344f61ff1fd34ffdb98bdeb17f
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2021-09-08 11:44:41 +0600

    Report

--- app/Http/Controllers/FinancialreportController.php
+++ app/Http/Controllers/FinancialreportController.php
@@ -62,14 +62,15 @@ class FinancialreportController extends Controller
                                 ->where('account_number','like', '%'. $request->input('account_number').'%')
                                 ->whereBetween('date',[$request->input('from'),$request->input('to')])
                                 ->get();
+        $data7= $request->input('from');
         $data6 = BankTransaction::select('date')
                                 ->groupBy('date')
                                 ->where('date',$request->input('to'))
                                 ->where('account_number','like', '%'. $request->input('account_number').'%')
                                 ->whereBetween('date',[$request->input('from'),$request->input('to')])
                                 ->get();
-
-        return view('backend.Financialreport.detailsreport',compact('data','data1','data2', 'data3','data4','data5','data6'));
+        $data8= $request->input('to');
+        return view('backend.Financialreport.detailsreport',compact('data','data1','data2', 'data3','data4','data5','data6','data7','data8'));
 
     }
 
